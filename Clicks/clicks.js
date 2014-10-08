fa19eb08c2e898294d17b4e915c286b1bfed4097
cynthiatsang <cynthiatsang28@gmail.com>
2014-10-07 18:08:59 -0700

    :)

--- Clicks/clicks.js
+++ Clicks/clicks.js
@@ -1,2 +1,8 @@
 /*global $*/
 /*jslint sloppy:true, browser: true*/
+$("h1.page-title").text("My code runs!");
+$(window).on('click', function (e) {
+    $('.logo').css({left: e.pageX,
+                    top: e.pageY});
+    $('.logo').toggleClass('rotated');
+});
